9a5cd8948b8fcc5ae2fe8eac8b45e5bc7b3c49d9
Anhelina Chernova <achernova3110@gmail.com>
2026-09-13 19:24:32 +0300

    Task 2

---
+++ oop-course/Lab01/Task2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab01
+{
+    public class Task2
+    {
+        public static void Run()
+        {
+            Console.Write("Enter price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter quantity: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter discount: ");
+            int discount = Convert.ToInt32(Console.ReadLine());
+
+            double sum = price * quantity * (1 - discount / 100.0);
+
+            Console.WriteLine("Сума: " + sum.ToString("F2") + " грн");
+        }
+    }
+}
+
