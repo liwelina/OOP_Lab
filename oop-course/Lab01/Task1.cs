f23c12afbffe6e75a501724d23255b3ee86a2525
Anhelina Chernova <achernova3110@gmail.com>
2026-09-13 19:24:10 +0300

    Task 1

---
+++ oop-course/Lab01/Task1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab01
+{
+    public class Task1
+    {
+        public static void Run()
+        {
+            Console.Write("Enter weight: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            height = height / 100;
+
+           double bmi = weight / (height * height);
+            Console.WriteLine("Body Mass Index: " + bmi.ToString("F2"));
+        }
+    }
+}  
