1499ba70f60df686bf9bfbfdb5f9e307c6a82128
Anhelina Chernova <achernova3110@gmail.com>
2026-09-13 19:25:50 +0300

    Task 7

---
+++ oop-course/Lab01/Task7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab01
+{
+    public class Task7
+    {
+        public static void Run()
+        {
+            Console.Write("Enter number of appointments: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Enter cost of appointment " + i + ": ");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                sum = sum + price;
+            }
+
+            double average = sum / n;
+
+            Console.WriteLine("Number of appointments: " + n);
+            Console.WriteLine("Total cost: " + sum);
+            Console.WriteLine("Average cost: " + average);
+        }
+    }
+}
+
+
