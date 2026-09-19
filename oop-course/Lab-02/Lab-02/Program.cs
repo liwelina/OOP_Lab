102b37a7dbbafe89df38a43595836b219f85daae
Anhelina Chernova <achernova3110@gmail.com>
2026-09-19 22:16:35 +0300

    Task4

--- oop-course/Lab-02/Lab-02/Program.cs
+++ oop-course/Lab-02/Lab-02/Program.cs
@@ -1,3 +1,3 @@
 ﻿using Lab02;
 
-Task3.Run();
+Task4.Run();
---
+++ oop-course/Lab-02/Lab-02/Task4.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Lab02
+{
+    public class Task4
+    {
+        public static void Run()
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            int m = int.Parse(Console.ReadLine()!);
+
+            int[,] matrix = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine()!.Split(' ');
+
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            int max = matrix[0, 0];
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < m; j++)
+                {
+                    sum += matrix[i, j];
+
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                        maxRow = i;
+                        maxCol = j;
+                    }
+                }
+
+                Console.WriteLine($"Doctor {i + 1}: {sum} appointments");
+            }
+
+            Console.Write("Day by day: ");
+
+            for (int j = 0; j < m; j++)
+            {
+                int sum = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    sum += matrix[i, j];
+                }
+
+                Console.Write(sum);
+
+                if (j < m - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Max: {max} (Doctor {maxRow + 1}, Day {maxCol + 1})");
+        }
+    }
+}
