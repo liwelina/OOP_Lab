4a7154368290e6f1fcbdceb3d73dffada748241e
Anhelina Chernova <achernova3110@gmail.com>
2026-09-19 21:15:49 +0300

    Task2

--- oop-course/Lab-02/Lab-02/Program.cs
+++ oop-course/Lab-02/Lab-02/Program.cs
@@ -1,3 +1,3 @@
 ﻿using Lab02;
 
-Task1.Run();
+Task2.Run();
---
+++ oop-course/Lab-02/Lab-02/Task2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab02
+{
+    public class Task2
+    {
+        public static void Run()
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            int[] queue = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                queue[i] = int.Parse(Console.ReadLine()!);
+            }
+
+            string before = string.Join(" ", queue);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (queue[j] > queue[j + 1])
+                    {
+                        int temp = queue[j];
+                        queue[j] = queue[j + 1];
+                        queue[j + 1] = temp;
+                    }
+                }
+            }
+
+            string after = string.Join(" ", queue);
+
+            Console.WriteLine($"Queue before: {before}");
+            Console.WriteLine($"Queue after: {after}");
+            Console.WriteLine($"Cheapest: {queue[0]} uah");
+            Console.WriteLine($"Most expensive: {queue[n - 1]} uah");
+        }
+    }
+}
